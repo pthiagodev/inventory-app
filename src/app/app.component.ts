25d0cb56ed33adc98afc3abfbf0e9eff7cec30d6
Thiago de Assis <pthiagodev@gmail.com>
2023-06-28 00:17:23 -0300

    altera nome do componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'inventory-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'inventory-app';
+  products: Product[];
+
+  constructor() {
+    this.products = [
+      new Product(
+        'MYSHOES',
+        'Black Running Shoes',
+        '/assets/images/products/black-shoes.jpg',
+        ['Men', 'Shoes', 'Running Shoes'],
+        109.99),
+      new Product(
+        'NEATOJACKET',
+        'Blue Jacket',
+        '/assets/images/products/blue-jacket.jpg',
+        ['Women', 'Apparel', 'jackets & Vests'],
+        238.99)
+    ];
+  }
+
+  productWasSelected(product: Product): void {
+    console.log(`Product clicked: ${product}`);
+    
+  }
 }
